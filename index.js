e688b4d12de0ceae5699bc2350a10445131d5343
Rousan Ali <ariyankhan46@gmail.com>
2017-10-31 14:16:17 +0530

    Updated comp

--- index.js
+++ index.js
@@ -2,5 +2,12 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-fusioncharts'
+  name: 'ember-fusioncharts',
+  description: 'A EmberJS binding for FusionCharts Charting library',
+  included: function(app) {
+    app.import('bower_components/fusioncharts/fusioncharts.js');
+    app.import('bower_components/fusioncharts/fusioncharts.charts.js');
+    app.import('bower_components/fusioncharts/themes/fusioncharts.theme.fint.js');
+    app.import('bower_components/fusioncharts/themes/fusioncharts.theme.ocean.js');
+  }
 };
